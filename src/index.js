b7d911ccaa4469a54e9e59cd3aedab6dd01a1f0b
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-06-30 20:12:48 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/docs", (req, res) => {
+  res.render("docs.ejs");
+});
+
+const { quotes, kataBijak } = require("./lib/data");
+
+app.get("/quoate", (req, res) => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  res.json({ quotes: randomQuote });
+});
+
+app.get("/katabijak", (req, res) => {
+  const randomBijak = kataBijak[Math.floor(Math.random() * kataBijak.length)];
+  res.json({ "kata bijak": randomBijak });
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
